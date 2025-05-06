1cf46b4c6ea9a86429490ed43082268c36b7d284
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-06 20:06:56 +0300

    fast commit for this prjrect

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <p className="read-the-docs text-2xl bg-amber-400 font-medium">
+      Click on the Vite and React logos to learn more
+    </p>
+  );
+}
+
+export default App;
