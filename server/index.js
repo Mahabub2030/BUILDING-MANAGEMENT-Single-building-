82aaf4f0b1637709e93b3fb2855cf781466b573f
Mahaub Alam <mahabubalom72091@gmail.com>
2025-06-06 13:56:54 +0300

    added all code to db

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ MongoClient.connect(uri, function (err, client) {
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
-    version: ServerApiVersion.v1,
+    version: ServerApiVersion.v1, 
     strict: true,
     deprecationErrors: true,
   },
